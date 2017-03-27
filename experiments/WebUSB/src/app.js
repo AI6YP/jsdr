bb2b5cc2ac5e80b5780b8d83647d4fe54ad33fb5
drom <alex.drom@gmail.com>
2017-03-26 23:44:15 -0700

    fixed WebUSB requestDevice

--- experiments/WebUSB/src/app.js
+++ experiments/WebUSB/src/app.js
@@ -1,6 +1,20 @@
 (function () {
-    navigator
-    .usb
-    .getDevices()
-    .then(dev => { console.log(dev); });
+
+    function wrapDevice (device) {
+        console.log(device);
+    }
+
+    const filters = [];
+
+    const btn = document.getElementById('connect');
+
+    btn.addEventListener('click', function () {
+        navigator.usb
+        .requestDevice({ 'filters': filters })
+        .then(device => wrapDevice(device));
+    });
+
 }());
+
+/* global navigator document */
+/* eslint no-console:0 */
